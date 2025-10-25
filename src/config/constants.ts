2e18f9bcdd0cb25f264305ca65f2b96275a10fa6
Asahi <bihruzen@gmail.com>
2025-10-25 19:05:33 +0300

    Revert "Add Farcaster friend invites"
    This reverts commit 883665e762ba9185f1f8cf08973a16f9ca7d1ba6.

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,6 +4,3 @@ export const APP_URL = import.meta.env.VITE_APP_URL ?? 'https://example.com'
 
 export const DEFAULT_CAPTURE_CONTRACT =
   '0xf4A396fA799e4A162Fb0df70d5957617D7061F27'
-
-export const FARCASTER_MINIAPP_URI =
-  import.meta.env.VITE_FARCASTER_MINIAPP_URI ?? 'farcaster://app/chessbase'
